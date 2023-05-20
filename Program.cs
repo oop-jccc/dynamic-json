5964c59498c9708c5aa16e0588c6ba9bfe864cf8
Damon German <damon.german@hyland.com>
2023-05-20 15:27:24 -0500

    made some changes

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace dynamic_json
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
