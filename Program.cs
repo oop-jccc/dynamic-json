e61afc27b9d57f36174c22ab3aaf8d5b16255100
Damon German <damon.german@hyland.com>
2023-09-30 12:14:58 -0500

    "Added TodoItem management feature to processing system"
    This commit introduces a new feature to the processing system that enables managing Todo items. A `TodoItem` record and a `TodoItemProcessor` class were added to allow the program to mark Todo items as completed. This enhancement adheres to the Open/Closed Principle to avoid modifying existing code. The dependency injection setup in the `Program` class, however, was updated to register the new `TodoItemProcessor` class. This design implements the Strategy Design Pattern, allowing interchangeable use of different processors through the `IJsonProcessor` interface.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,36 @@
 using dynamic_json.JsonProcessors;
 
-namespace dynamic_json
+namespace dynamic_json;
+
+public static class Program
 {
-    public static class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IJsonProcessor, UserJsonProcessor>();
+        builder.Services.AddSingleton<IJsonProcessor, ProductJsonProcessor>();
+        builder.Services.AddSingleton<IJsonProcessor, TodoItemProcessor>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddSingleton<IJsonProcessor, UserJsonProcessor>();
-            builder.Services.AddSingleton<IJsonProcessor, ProductJsonProcessor>();
-            builder.Services.AddSingleton<IJsonProcessor, TodoItemProcessor>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
